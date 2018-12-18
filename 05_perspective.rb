8b1cb8e76b758330d78bfc7abff110b63f900b8d
Pamela Kwong <pamelaskwong@gmail.com>
2018-12-17 21:35:01 -0500

    Enter

---
+++ 05_perspective.rb
@@ -0,0 +1,9 @@
+puts "assumptions" < "change"
+
+
+if "assumptions" < "change"
+    puts "You accept change and do not make assumptions of other people."
+else
+    puts "You do not accept change and only make assumptions of the present and future."
+end
+
