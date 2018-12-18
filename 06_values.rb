be26c2d55eb3fea090e89dcb94435301bc008f3a
Pamela Kwong <pamelaskwong@gmail.com>
2018-12-18 10:09:09 -0500

    Enter

---
+++ 06_values.rb
@@ -0,0 +1,13 @@
+def the_five_p_s(practice)
+
+    my_values = [
+        "patience", 
+        "peace", 
+        "perseverance", 
+        "persistence", 
+        "poise"
+    ]
+    return my_values[practice]
+end
+
+p the_five_p_s(1)
