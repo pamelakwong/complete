baf48abc83ef0d1917a7219d7074792bf63bd4ff
pamelakwong <pamelaskwong@gmail.com>
2019-04-22 23:18:05 -0400

    ADD tentative methods

--- balanced_life/well_being.rb
+++ balanced_life/well_being.rb
@@ -1,1 +1,15 @@
 # To understand the world is just as important as understanding the mind.
+class Well_Being 
+    # At the end of each day, acknowledge what you accomplished.
+    def recap 
+
+    end
+
+    # Remember that nothing will every be prefect, but you can be satisfied that you worked with integrity and made a strong effort. 
+    def imperfection  
+        
+    end
+
+    # After recapping, pat yourself on the back
+
+end
