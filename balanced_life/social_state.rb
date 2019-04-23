4cb9478849f179a0c029cfee8779eefb5c85cd57
pamelakwong <pamelaskwong@gmail.com>
2019-04-23 01:28:54 -0400

    MODIFY methods and ADD attributes

--- balanced_life/social_state.rb
+++ balanced_life/social_state.rb
@@ -1,6 +1,10 @@
+require_relative "emotional_state"
+
 class Social_State
-    def initialize 
+    attr_reader :emotional_state, :network
+    def initialize(emotional_state = Emotional_State.new)
         @network = {}
+        @emotional_state = emotional_state
     end
 
     def make_friends(friend)
@@ -8,14 +12,17 @@ class Social_State
     end
 
     def converse 
-
+        emotional_state.laugh 
     end
 
     def help_others 
+        emotional_state.encourage_each_other
     end
 
     def voice_vulnerability 
-
+        if emotional_state.cowering 
+            puts "Reach out to someone. You are not alone."
+        end
     end
 
 end
