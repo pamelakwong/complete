4a08e8dcf56774f62e376c6b7ae5c3b9b61c92e4
pamelakwong <pamelaskwong@gmail.com>
2019-04-23 01:22:08 -0400

    MODIFY methods and attributes

--- balanced_life/emotional_state.rb
+++ balanced_life/emotional_state.rb
@@ -1,15 +1,37 @@
+require "byebug"
 require_relative "emotions"
 class Emotional_State
+    attr_reader :suppressed_feelings
+    attr_accessor :surfacing
     def initialize
         @surfacing = Emotions.new(1, 2, 3, 4, 5)
 
         # will be in private => helper method: resurfaced_feelings 
+        # use a % on sadness and put the remainder into suppressed_feelings 
         @suppressed_feelings = 0 
+    end
+
+    def balanced
+        @surfacing.anger = 0 
+        @surfacing.fear = 0 
+        @surfacing.disgust = 0 
+        @surfacing.sadness = 0 
+        @surfacing.enjoyment = 0 
+    end
+
+    def balanced? 
+        @surfacing.average_feeling == 0.0 
     end
 
     def imbalanced_feelings?
         @surfacing.average_feeling < 8.6 
     end
+
+    # What do we do when we reflect? How to express reflection without reflecting? 
+    def reflect 
+        balanced
+    end
+
 
     #----------------------------------
     # Measure the lows and extremes of each emotion
@@ -17,56 +39,49 @@ class Emotional_State
     # How does this radar relate to other Classes?
     # OH! MUST INCLUDE THIS METHOD IN WELL_BEING 
     def radar 
-
+    
     end
 
     # Have to consider which negative & positive emotions are inserted into the following methods.
     # an emotion that has an exceptionally high #
     def alert
-
+        reflect if suppressed_feelings > 10 
     end
 
     # an emotion that has a low # 
-    def okay 
-        
+    def its_okay 
+        encourage_each_other if @surfacing.sadness > 5
     end
 
     #----------------------------------
-    # Use #radar, #alert, #okay to justify and define the methods below 
 
     def pat_myself_on_the_back
-        @enjoyment += 11
+        @surfacing.enjoyment += 1 if alert 
         puts "You've done well. You worked hard!"
     end
 
     # We’re all proud of the progress you made each and every day! 
-    def encourage 
-        @enjoyment += 12
+    def encourage_each_other
+        @surfacing.enjoyment += 1
         puts "Keep going. Keep moving forward. What matters is taking action and putting forth the action."
     end
 
-    # What do we do when we reflect? How to express reflection without reflecting? 
-    def reflect 
-        @anger -= 11
-        @fear -= 11
-        @disgust -= 11
-        @sadness -= 101
-        @enjoyment += 1
-    end
-
-    def shed_tears 
+    def shed_tears(tears)
         # Must increment endorphins in Physical_State Class. 
         puts "You have shed tears. It is okay to cry. When you cry you also release endorphins."
-        @sadness -= 11
-        @enjoyment += 1
+        if @surfacing.sadness + tears >= 11 
+            @suppressed_feelings += (@surfacing.sadness + tears) % 11
+        end
+        @surfacing.sadness = 11
+        @surfacing.enjoyment += 1
 
     end 
 
     def laugh 
-        @enjoyment += 12
+        @surfacing.enjoyment += 1
     end
 
-    def suppresing_feelings
+    def cowering 
         @suppressed_feelings += 1
     end
 end
