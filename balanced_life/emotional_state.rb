2609dfdef8dacfb987030e19bef2f46ac87a4af7
pamelakwong <pamelaskwong@gmail.com>
2019-04-22 23:16:15 -0400

    ADD #radar method and modify other methods

--- balanced_life/emotional_state.rb
+++ balanced_life/emotional_state.rb
@@ -1,6 +1,7 @@
 class Emotional_State
     attr_accessor :anger, :fear, :disgust, :sadness, :enjoyment
     def initialize(anger, fear, disgust, sadness, enjoyment)
+        # put emotions into a hash? 
         @anger = anger 
         @fear = fear 
         @disgust = disgust 
@@ -11,20 +12,12 @@ class Emotional_State
         @suppressed_feelings = 0 
     end
 
-    def pat_myself_on_the_back
-        @enjoyment += 11
-    end
+    # Measure the lows and extremes of each emotion
+    # Relate to The Atlas of Emotions
+    # How does this radar relate to other Classes?
+    # OH! MUST INCLUDE THIS METHOD IN WELL_BEING 
+    def radar 
 
-    def encourage 
-        @enjoyment += 12
-    end
-
-    def reflect 
-        @anger -= 11
-        @fear -= 11
-        @disgust -= 11
-        @sadness -= 101
-        @enjoyment += 1
     end
 
     def imbalanced_feelings?
@@ -33,8 +26,28 @@ class Emotional_State
         avg > 50 ? "take a break" : shed_tears 
     end
 
+    def pat_myself_on_the_back
+        @enjoyment += 11
+        puts "You've done well. You worked hard!"
+    end
+
+    # We’re all proud of the progress you made each and every day! 
+    def encourage 
+        @enjoyment += 12
+        puts "Keep going. Keep moving forward. What matters is taking action and putting forth the action."
+    end
+
+    # What do we do when we reflect? How to express reflection without reflecting? 
+    def reflect 
+        @anger -= 11
+        @fear -= 11
+        @disgust -= 11
+        @sadness -= 101
+        @enjoyment += 1
+    end
+
     def shed_tears 
-        # Must increment endorphins in Social_State Class. 
+        # Must increment endorphins in Physical_State Class. 
         puts "You have shed tears. It is okay to cry. When you cry you also release endorphins."
         @sadness -= 11
         @enjoyment += 1
