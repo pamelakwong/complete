c4b7b57e1948439d4b25a1215858e007f907672e
pamelakwong <pamelaskwong@gmail.com>
2019-04-23 00:01:36 -0400

    INITIAL commit and ADD Class variables

---
+++ balanced_life/emotions.rb
@@ -0,0 +1,67 @@
+class Emotions 
+# Defined stages for each emotion are derived from the website The Atlas of Emotions supported by Dalai Lama     
+
+    # annoyance, frustration, exasperation, argumentativeness, bitterness, vengefulness, fury
+    ANGER_STAGES = {
+        1 => "annoyance", 
+        2 => "frustration", 
+        3 => "exasperation",
+        4 => "argumentativeness", 
+        5 => "bitterness", 
+        6 => "fury"
+    }
+
+    # trepidation, nervousness, anxiety, dread, desperation, panic, horror, terror
+    FEAR_STAGES = {
+        1 => "trepidation", 
+        2 => "nervousness", 
+        3 => "anxiety",
+        4 => "dread", 
+        5 => "desperation", 
+        6 => "panic",
+        7 => "horror", 
+        8 => "terror"
+    }
+
+    # dislike, aversion, distaste, repugnance, revulsion, abhorrence, loathing
+    DISGUST_STAGES = {
+        1 => "dislike", 
+        2 => "aversion", 
+        3 => "distaste",
+        4 => "repugnance", 
+        5 => "revulsion", 
+        6 => "abhorrence",
+        7 => "loathing", 
+    }
+
+    # disappointment, discouragement, distraughtness, resignation, helplessness, hopelessness, misery, despair, grief, sorrow, anguish
+    SADNESS_STAGES = {
+        1 => "disappointment", 
+        2 => "discouragement", 
+        3 => "distraughtness",
+        4 => "resignation", 
+        5 => "helplessness", 
+        6 => "hopelessness",
+        7 => "misery", 
+        8 => "despair", 
+        9 => "grief", 
+        10 => "sorrow", 
+        11 => "anguish"    
+    }
+
+    # sensory pleasure, rejoicing, compassion/joy, amusement/schadenfreude, relief, peace, fiero, pride, naches, wonder, excitement, ecstasy
+    ENJOYMENT_STAGES = {
+        1 => "sensory pleasure", 
+        2 => "rejoicing", 
+        3 => "compassion/joy",
+        4 => "amusement/schadenfreude", 
+        5 => "relief", 
+        6 => "peace",
+        7 => "fiero", 
+        8 => "pride", 
+        9 => "naches", 
+        10 => "wonder", 
+        11 => "excitement"  
+    }
+
+end
