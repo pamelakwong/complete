2f654f306e19b207c70da8cd494b5c3f777c8fdf
pamelakwong <pamelaskwong@gmail.com>
2019-04-22 22:10:11 -0400

    ADD attributes and methods

--- balanced_life/physical_state.rb
+++ balanced_life/physical_state.rb
@@ -1,24 +1,45 @@
 class Physical_State
-    def intialize 
-        @hours_of_sleep = 8
-        @lunch = true
+    attr_accessor :hours_of_sleep, :lunch, :breaks, :shower, :endorphins
+    def initialize(hours_of_sleep, lunch, breaks, shower)
+        @hours_of_sleep = hours_of_sleep
+        @lunch = lunch 
+        @breaks = breaks
+        @shower = shower  
         @endorphins = 0
     end
 
-    def exercise 
+    def sleep 
+        raise "GET SLEEP" if hours_of_sleep <= 0 
+        if hours_of_sleep > 0 && hours_of_sleep < 4
+            puts "It's great that you have sleep, but get some more! (: "
+        end
+
+        if hours_of_sleep > 4 
+            puts "Good job! You are getting adequate sleep!"
+        end
+    end
+
+    def ate? 
+        lunch == true 
+    end
+
+    def take_breaks? 
+        if !(ate? && breaks == 2) 
+            puts "Remember to rest your eyes once awhile. The blue rays emitting from the screen is unhealthy. Also, give your brain a rest from working too hard! (: "
+            false 
+        end
+        true 
+    end
+
+    def exercise? 
         @endorphins += 100
     end
 
-    def shower 
-
+    def clean? 
+        shower
     end
 
     def dress_up 
         #weather #temp 
     end
-
-    def sleep 
-
-    end
-
 end
