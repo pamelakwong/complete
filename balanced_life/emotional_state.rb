33c7e01256775a064f2cf4c0b618f44ee9771fa1
pamelakwong <pamelaskwong@gmail.com>
2019-04-23 03:48:19 -0400

    CLEAN comments and methods

--- balanced_life/emotional_state.rb
+++ balanced_life/emotional_state.rb
@@ -7,7 +7,6 @@ class Emotional_State
         @surfacing = Emotions.new(1, 2, 3, 4, 5)
 
         # will be in private => helper method: resurfaced_feelings 
-        # use a % on sadness and put the remainder into suppressed_feelings 
         @suppressed_feelings = 0 
     end
 
@@ -27,33 +26,21 @@ class Emotional_State
         @surfacing.average_feeling < 8.6 
     end
 
-    # What do we do when we reflect? How to express reflection without reflecting? 
     def reflect 
         balanced
     end
 
-
-    #----------------------------------
-    # Measure the lows and extremes of each emotion
-    # Relate to The Atlas of Emotions
-    # How does this radar relate to other Classes?
-    # OH! MUST INCLUDE THIS METHOD IN WELL_BEING 
-    def overflowing_emotions
-        @surfacing.average_feeling > 8.6 
-    end
-
-    # Have to consider which negative & positive emotions are inserted into the following methods.
-    # an emotion that has an exceptionally high #
     def alert
         reflect if suppressed_feelings > 10 
     end
 
-    # an emotion that has a low # 
-    def its_okay 
-        encourage_each_other if @surfacing.sadness > 5
+    def overflowing_emotions
+        @surfacing.average_feeling > 5.0 
     end
 
-    #----------------------------------
+    def its_okay 
+        encourage_each_other if overflowing_emotions
+    end
 
     def pat_myself_on_the_back
         @surfacing.enjoyment += 1 if alert 
@@ -63,7 +50,7 @@ class Emotional_State
     # We’re all proud of the progress you made each and every day! 
     def encourage_each_other
         @surfacing.enjoyment += 1
-        puts "Keep going. Keep moving forward. What matters is taking action and putting forth the action."
+        puts "What matters is taking action! Keep going. Keep moving forward."
     end
 
     def shed_tears(tears)
@@ -74,7 +61,6 @@ class Emotional_State
         end
         @surfacing.sadness = 11
         @surfacing.enjoyment += 1
-
     end 
 
     def laugh 
